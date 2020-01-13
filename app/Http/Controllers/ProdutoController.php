e110ff3462b9c907faec733a5f45f92d752af644
Joao Lucas Amorim Dev <jlamorimdev@gmail.com>
2020-01-13 10:37:07 -0300

    Criado Produto Controller, criado rota para controller

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace sistemaLaravel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use sistemaLaravel\Produto;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Input;
+use sistemaLaravel\Http\Requests\ProdutoFormRequest;
+use DB;
+
+class ProdutoController extends Controller
+{
+  public function __construct(){
+
+  }
+
+    public function index(Request $request)
+    {
+      if ($request) {
+        $query = trim($request->get('searchText'));
+        $produtos=DB::table('produto as p')
+                        ->join('categoria as c',
+                               'p.idcategoria', '=', 'c.idcategoria')
+                        ->select('p.idproduto', 'p.nome', 'p.codigo', 'p.estoque',
+                                  'c.nome as categoria', 'p.descricao', 'p.imagem', 'p.estado')
+                        ->where('p.nome', 'LIKE' , '%' .$query . '%')
+                        ->orderBy('idproduto', 'desc')
+                        ->paginate(5);
+
+        return view("estoque.produto.index", [
+          "produtos" => $produtos,
+          "searchText" => $query
+        ]);
+      }
+    }
+
+    public function create()
+    {
+      $categorias=DB::table('categoria')
+                    ->where('condicao', "=" , '1')
+                    ->get();
+        return view("estoque.produto.create", [
+          "categorias" => $categorias
+        ]);
+    }
+
+    public function store(ProdutoFormRequest $request)
+    {
+        $produto = new Produto;
+        $produto->idcategoria = $request->get('idcategoria');
+        $produto->codigo = $request->get('codigo');
+        $produto->nome = $request->get('nome');
+        $produto->estado = 'Ativo';
+
+        if (Input::hasFile('imagem')) {
+          $file = Input::file('imagem');
+          $file->move(public_patch().'/imagens/produtos/',
+          $file->getClienteOriginalName());
+          $produto->imagem = $file->getClienteOriginalName();
+        }
+
+        $produto->save();
+
+        return Redirect::to('estoque/produto');
+    }
+
+    public function show($id)
+    {
+        return view("estoque.produto.show", [
+          "produto" => Produto::findOrFail($id)
+        ]);
+    }
+
+    public function edit($id)
+    {
+      $produto = Produto::findOrFail($id);
+      $categorias = DB::table('categoria')
+                        ->where('condicao', '=', '1')
+                        ->get();
+
+        return view("estoque.produto.edit", [
+              "produto" => $produto,
+              "categorias" => $categorias
+        ]);
+    }
+
+
+    public function update(ProdutoFormRequest $request, $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->idcategoria = $request->get('idcategoria');
+        $produto->codigo = $request->get('codigo');
+        $produto->nome = $request->get('nome');
+
+        if (Input::hasFile('imagem')) {
+          $file = Input::file('imagem');
+          $file->move(public_path().'/imagens/produtos/',
+          $file->getClientOriginalName());
+          $produto->imagem = $file->getClientOriginalName();
+        }
+
+        $produto->update();
+
+        return Redirect::to('estoque/produto');
+    }
+
+    public function destroy($id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->estado = 'Inativo';
+        $produto->update();
+
+        return Redirect::to('estoque/produto');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::resource('estoque/categoria', 'CategoriaController');
+Route::resource('estoque/produto', 'ProdutoController');
